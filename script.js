5f568872424647425126f8e1dd29da46c501d82e
Laxmi-sahani8 <165988964+Laxmi-sahani8@users.noreply.github.com>
2025-03-26 23:13:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById("changeColorBtn").addEventListener("click", function() {
+    // Random color generation
+    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+});
